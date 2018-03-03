b9d4bfd6c67595ec783dd8136a435a3d803ef938
Julie <jcpersin@ncsu.edu>
2018-03-03 02:18:35 -0500

    Add character movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	public Transform CharacterTransform;
+	private int MovementChange = 3;
+	private InputManager CurrentInstance;
+
+	// Use this for initialization
+	void Start ()
+	{
+		CurrentInstance = InputManager.Instance;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float horizontalPosition = CurrentInstance.GetCompDevInputAxis(CompDevInput.DirectionHorizontal);
+		float verticalPosition = CurrentInstance.GetCompDevInputAxis(CompDevInput.DirectionVertical);
+		Vector3 newPosition = new Vector3(horizontalPosition, verticalPosition);
+		CharacterTransform.position += newPosition * MovementChange;
+	}
+}
